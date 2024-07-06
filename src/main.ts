41a5009a83362d86f413716f13ebc3c037f6c93c
Ayomi <nikitakabo74@gmail.com>
2024-07-06 16:40:39 +0300

    const app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { router } from './router/router'
-import './main.css'
-import i18n from './i18n'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { router } from "./router/router";
+import "./main.css";
+import i18n from "./i18n";
 
-createApp(App).use(i18n).use(router).mount('#app')
+const app = createApp(App);
+app.use(i18n).use(router).mount("#app");
